5b1dcbbd3ac5239484dfebc1649d15e4aa868ef1
Christian Knapp <christian.alex.knapp@icloud.com>
2024-05-31 23:26:36 +0200

    added enderdragon condition (timer stops by killing the enderdragon)

---
+++ src/main/java/io/github/schmolldechse/challenge/map/condition/EnderDragonCondition.java
@@ -0,0 +1,75 @@
+package io.github.schmolldechse.challenge.map.condition;
+
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import io.github.schmolldechse.Plugin;
+import io.github.schmolldechse.challenge.Challenge;
+import io.github.schmolldechse.challenge.Identification;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EnderDragonCondition extends Challenge {
+
+    private final Plugin plugin;
+
+    public EnderDragonCondition() {
+        super("condition_enderdragon");
+
+        this.plugin = JavaPlugin.getPlugin(Plugin.class);
+    }
+
+    @Override
+    public Identification challengeIdentification() {
+        return Identification.CONDITION;
+    }
+
+    @Override
+    public ItemStack getItemStack() {
+        return ItemBuilder.from(Material.END_STONE)
+                .name(this.getDisplayName())
+                .lore(this.getDescription())
+                .pdc(persistentDataContainer -> persistentDataContainer.set(this.key, PersistentDataType.STRING, this.getIdentifierName()))
+                .build();
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.text("Ender Drache", NamedTextColor.RED).decoration(TextDecoration.ITALIC, false);
+    }
+
+    @Override
+    public List<Component> getDescription() {
+        Component activated = this.active
+                ? Component.text("Aktiviert", NamedTextColor.GREEN)
+                : Component.text("Deaktiviert", NamedTextColor.RED);
+
+        return Arrays.asList(
+                Component.empty(),
+                Component.text("Die Challenge gilt als geschlagen,", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false),
+                Component.text("wenn der Ender Drache geschlagen wird", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false),
+                Component.empty(),
+                activated
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void execute(EntityDeathEvent event) {
+        if (!this.active) return;
+        if (this.plugin.timerHandler.isPaused()) return;
+
+        if (!(event.getEntity() instanceof EnderDragon)) return;
+
+        this.success();
+    }
+}
