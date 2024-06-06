b06d6d4ab260cd77d743ceffdc9a3c17f1c89149
Christian Knapp <christian.alex.knapp@icloud.com>
2024-06-07 00:34:11 +0200

    added only hotbar challenge

---
+++ src/main/java/io/github/schmolldechse/challenge/map/player/HotbarSetting.java
@@ -0,0 +1,82 @@
+package io.github.schmolldechse.challenge.map.player;
+
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import io.github.schmolldechse.challenge.Challenge;
+import io.github.schmolldechse.challenge.Identification;
+import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class HotbarSetting extends Challenge implements Listener {
+
+    public HotbarSetting() {
+        super("setting_hotbar");
+
+        this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+    }
+
+    @Override
+    public Identification challengeIdentification() {
+        return Identification.PLAYER;
+    }
+
+    @Override
+    public ItemStack getItemStack() {
+        return ItemBuilder.from(Material.SKELETON_SKULL)
+                .name(this.getDisplayName())
+                .lore(this.getDescription())
+                .pdc(persistentDataContainer -> persistentDataContainer.set(this.key, PersistentDataType.STRING, this.getIdentifierName()))
+                .build();
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.text("Nur Hotbar", NamedTextColor.RED).decoration(TextDecoration.ITALIC, false);
+    }
+
+    @Override
+    public List<Component> getDescription() {
+        return Arrays.asList(
+                Component.empty(),
+                Component.text("Nur die Hotbar kann verwendet werden!", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false),
+                Component.empty(),
+                this.activationComponent()
+        );
+    }
+
+    @Override
+    public Map<String, Object> save() {
+        return super.save();
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void execute(PlayerInventorySlotChangeEvent event) {
+        if (!this.active) return;
+        if (this.plugin.timerHandler.isPaused()) return;
+
+        if (event.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
+
+        // 1. slot starts with 0, so last is 8
+        if (event.getSlot() < 9) return;
+
+        Bukkit.broadcast(
+                Component.text(event.getPlayer().getName(), NamedTextColor.GREEN)
+                        .append(Component.text(" hatte ein Item außerhalb der Hotbar", NamedTextColor.RED))
+        );
+
+        this.fail();
+    }
+}
